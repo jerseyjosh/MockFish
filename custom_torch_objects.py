86a4dd7b33a4dcbee6ba445e5a8639de079b491d
Josh Griffiths <josh@Joshs-Macbook-2.local>
2022-06-22 16:54:05 +0100

    smaller architecture

--- custom_torch_objects.py
+++ custom_torch_objects.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import ToTensor
+from torchvision.transforms import ToTensor, Normalize, Compose
 import torch.nn as nn
 from config import *
 
 class ChessDataset(Dataset):
-    def __init__(self, root, path, transforms=ToTensor()):
+    def __init__(self, root, path, transforms=Compose([ToTensor(), Normalize()])):
         self.path = path
         self.transforms=transforms
         self.df = pd.read_pickle(DATA_DIR + DF_PATH) 
@@ -27,28 +27,21 @@ class ChessDataset(Dataset):
         return board_state, from_square, to_square, piece_moved
     
 
-
 class Mockfish(nn.Module):
     def __init__(self, numChannels, classes):
         super(Mockfish, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels = 6, out_channels=20, kernel_size=(3, 3))
+        self.conv1 = nn.Conv2d(in_channels = 6, out_channels=10, kernel_size=(3, 3))
         self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=(5, 5))
-        self.relu2 = nn.ReLU()
-        self.fc1 = nn.Linear(in_features=200, out_features=100)
+        self.fc1 = nn.Linear(in_features=360, out_features=180)
         self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(in_features=100, out_features=classes)
-        self.logsoftmax = nn.LogSoftmax(dim=1)
+        self.fc2 = nn.Linear(in_features=180, out_features=classes)
     
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.relu3(x)
-        x = self.fc2(x)
-        output = self.logsoftmax(x)
+        output = self.fc2(x)
         return output
     
